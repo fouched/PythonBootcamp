cfea8450f73efafcc056d270b37f28e885e6841f
Fouche du Preez <fouche@limehouse.co.za>
2024-09-05 22:27:47 +0200

    learning

---
+++ Day001/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("Where were you born?\n")
+pet = input("What is the name of your 1st pet?\n")
+print("Your band name could be: " + city + pet)
