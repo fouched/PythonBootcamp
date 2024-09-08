c6681516f657f68ef7d8c6f21b08431fa299760d
Fouche du Preez <fouche@limehouse.co.za>
2024-09-08 21:23:08 +0200

    day 4

---
+++ Day004/task.py
@@ -0,0 +1,33 @@
+import random
+# import mymodule
+# from mymodule import num
+# from mymodule import num as nr
+
+# r = random.randint(1,10)
+# print(r)
+#
+# print(num)
+
+# nr = random.random() * 10
+# print(nr)
+
+# r = random.randint(0,1)
+# if r == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# states = ["Alabama", "Alaska", "Arizona", "Arkansas"]
+# states.append("California")
+
+# print(states[4])
+# print(states)
+
+# nr = random.randint(0, len(states)-1)
+# print(states[nr])
+# print(random.choice(states))
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries"]
+vegetables = ["Spinach", "Kale", "Tomatoes" "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
