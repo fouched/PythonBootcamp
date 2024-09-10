3e57f2fc537a31ba0be52b9c6d878f6443f4c30f
Fouche du Preez <fouche@limehouse.co.za>
2024-09-10 06:35:10 +0200

    day 5

---
+++ Day005/task.py
@@ -0,0 +1,39 @@
+# fruits = ["Apple", "Peach", "Pear"]
+#
+# for fruit in fruits:
+#     print(fruit)
+
+# student_scores = [45, 50, 75, 89, 90, 45, 60]
+#
+# print(sum(student_scores))
+# print(max(student_scores))
+#
+# max_score = 0
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
+#
+# print(max_score)
+
+# for num in range(1, 10):
+#     print(num)
+#
+# for num in range(1, 10, 3):
+#     print(num)
+#
+# total = 0
+# for num in range(1, 101):
+#     total += num
+#
+# print(total)
+
+# for num in range(1, 101):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("FizzBuzz")
+#     elif num % 3 == 0:
+#         print("Fizz")
+#     elif num % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(num)
+
