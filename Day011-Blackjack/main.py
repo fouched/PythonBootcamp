f191e1b8d4b5eaa5996c05727febd0fe39a5a685
Fouche du Preez <fouche@limehouse.co.za>
2024-09-21 14:09:04 +0200

    day 11

--- Day011-Blackjack/main.py
+++ Day011-Blackjack/main.py
@@ -11,33 +11,21 @@ comp_cards = []
 def deal() -> int:
     return random.choice(cards)
 
-def start_game():
+def start_game() -> bool:
     player_cards.clear()
     player_cards.append(deal())
     player_cards.append(deal())
+    print("P:", player_cards)
 
     comp_cards.clear()
     comp_cards.append(deal())
     comp_cards.append(deal())
+    print("C:", comp_cards[0])
 
-
-def play_game() -> bool:
-    user_total = check_total(player_cards)
-    print("U:", player_cards, user_total)
-
-    check_total(comp_cards)
-    comp_total = check_total(comp_cards)
-    print("C:", comp_cards, comp_total)
-
-    if comp_total == 21:
-        print("Computer wins")
+    if check_total(comp_cards) == 21:
         return False
-
-    if user_total == 21:
-        print("You win!!!")
-        return False
-
-    return True
+    else:
+        return True
 
 
 def check_total(c: []) -> int:
@@ -47,19 +35,80 @@ def check_total(c: []) -> int:
 
     return total
 
-start_game()
-while play_game():
+
+def player_turn() -> bool:
     i = input("Deal a card? (y/n): ").lower()
     if i == "y":
-        player_cards.append(deal())
-        print(player_cards)
+        card = deal()
+        player_cards.append(card)
+        return True
     else:
-        user_total = check_total(player_cards)
-        comp_total = check_total(comp_cards)
-        if user_total > comp_total:
-            print("You win!!!")
-            break
-        else:
-            print("Computer wins")
-            break
+        return False
+
+
+def computer_turn() -> bool:
+    comp_total = check_total(comp_cards)
+    if comp_total < 17:
+        card = deal()
+        comp_cards.append(card)
+        return True
+    else:
+        return False
+
+def print_final_score():
+    computer_total = check_total(comp_cards)
+    player_total = check_total(player_cards)
+
+    print("")
+    print("==============================")
+    print("Player cards:", player_cards)
+    print("Computer cards:", comp_cards)
+    print(f"Final score: P:{player_total} C:{computer_total}")
+    print("==============================")
+
+    if player_total > 21:
+        print("Computer wins :-(")
+    elif computer_total > 21:
+        print("You win!!!")
+    elif player_total == computer_total:
+        print("Draw")
+    elif player_total > computer_total:
+        print("You win!!!")
+    else:
+        print("Computer wins :-(")
+
+    print("==============================")
+
+
+def play_game():
+    cont_game = start_game()
+
+    if cont_game:
+        while player_turn():
+            print("Player:", player_cards)
+            if check_total(player_cards) > 21:
+                print("You bust!")
+                break
+
+        if check_total(player_cards) <= 21:
+            while computer_turn():
+                print("Computer:", comp_cards)
+                if check_total(comp_cards) > 21:
+                    print("Computer bust!")
+                    break
+
+    print_final_score()
+
+    play_again = input("Play again? (y/n): ").lower()
+    if play_again == "y":
+        print("\n" * 20)
+        play_game()
+    else:
+        print("Goodbye!")
+
+
+play_game()
+
+
+
 
