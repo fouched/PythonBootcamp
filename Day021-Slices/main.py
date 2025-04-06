bfa15c4c60a500a254eca0d6713715c1c40dd40c
Fouche du Preez <fouche@limehouse.co.za>
2025-04-06 19:30:35 +0200

    day 21

---
+++ Day021-Slices/main.py
@@ -0,0 +1,23 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+# get elements 2 to 5
+print(piano_keys[2:5])
+print(piano_tuple[2:5])
+
+# get all elements from 2
+print(piano_keys[2:])
+print(piano_tuple[2:])
+
+# get first 5 elements
+print(piano_keys[:5])
+print(piano_tuple[:5])
+
+# get every 2nd element
+print(piano_keys[2:7:2])
+print(piano_tuple[2:7:2])
+
+# reverse a list
+print(piano_keys[::-1])
+print(piano_tuple[::-1])
+
